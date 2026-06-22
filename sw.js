d82a5e78c9ab2913368113a43e628b32342749b7
betokasa1982-del <betokasa1982@gmail.com>
2026-06-22 08:27:42 +0200

    icon correction

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
-const CACHE='dcp-v2';
+const CACHE='dcp-v3';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{ self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html'])).catch(()=>{})); });
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{})); });
 self.addEventListener('activate',e=>{ e.waitUntil(
   caches.keys().then(ks=>Promise.all(ks.map(k=>k===CACHE?null:caches.delete(k)))).then(()=>self.clients.claim())); });
 self.addEventListener('fetch',e=>{
@@ -11,5 +12,8 @@ self.addEventListener('fetch',e=>{
               .catch(()=> caches.match('./index.html').then(x=> x || caches.match('./'))) );
     return;
   }
-  e.respondWith( caches.match(r).then(hit=> hit || fetch(r)) );
+  e.respondWith( caches.match(r).then(hit=> hit || fetch(r).then(res=>{
+    try{ if(new URL(r.url).origin===location.origin){ const cp=res.clone(); caches.open(CACHE).then(c=>c.put(r,cp)); } }catch(_){}
+    return res;
+  })) );
 });
